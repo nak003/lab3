55bbab69c52b9b77dd06c3e11495581f67c2e452
nak003 <nak003@ucsd.edu>
2022-10-13 12:31:52 -0700

    add

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,11 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+  public void testReverseInPlace1(){
+    int[] input1 = {1,2,3};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{3,2,1}, input1);
+  }
 }
